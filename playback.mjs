5d6ddac984a47cd0248570caca8db14d3e2af0e4
Mark Pomerant <mpomerant@upchain.com>
2021-11-18 11:34:21 -0500

    separate subscription from playback route

--- playback.mjs
+++ playback.mjs
@@ -1,5 +1,7 @@
 import express from 'express';
 import fetch from 'node-fetch';
+import subscription from './subscription.mjs';
+
 const router = express.Router({mergeParams: true});
 
 router.post('/play', async(req, res) => {
@@ -82,76 +84,8 @@ router.post('/pause', async(req, res) => {
     res.json(json);
 });
 
-router.post('/subscription', async(req, res) => {
+router.use('/subscription', subscription);
 
-    const accessToken = req.session.accessToken
-    const groupId = req.params.id;
-    console.log(`subscription: ${groupId}`);
-    
-    const response =  await fetch(`https://api.ws.sonos.com/control/api/v1/groups/${groupId}/playback/subscription`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`
-        }
-    });
-     const metadataResponse =  await fetch(`https://api.ws.sonos.com/control/api/v1/groups/${groupId}/playbackMetadata/subscription`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`
-        }
-    });
-    const currentState =  await fetch(`https://api.ws.sonos.com/control/api/v1/groups/${groupId}/playback`, {
-        method: 'GET',
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`
-        }
-    });
-
-   const currentStateJson = await currentState.json();
-   const mdJson = await metadataResponse.json();
-   const json = await response.json();
-   console.log(json);
-   console.log('--------------------------');
-   console.log(currentStateJson);
-
-   res.json(currentStateJson);
-});
-
-router.delete('/subscription', async(req, res) => {
-
-    const accessToken = req.session.accessToken
-    const groupId = req.params.id;
-    
-    if (groupId) {
-        console.log(`unsubscribing from ${groupId}`);
-        const response =  await fetch(`https://api.ws.sonos.com/control/api/v1/groups/${groupId}/playback/subscription`, {
-        method: 'DELETE',
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`
-        }
-        });
-        const metadataResponse =  await fetch(`https://api.ws.sonos.com/control/api/v1/groups/${groupId}/playbackMetadata/subscription`, {
-            method: 'DELETE',
-            headers: {
-                "Content-Type": "application/json",
-                Authorization: `Bearer ${accessToken}`
-            }
-        });
-        const mdJson = await metadataResponse.json();
-        const json = await response.json();
-        console.log(json);
-        console.log(`metadata: ${mdJson}`);
-    
-        res.json(json);
-    }
-    
-
-  
-})
 
 router.get('/', async(req, res) => {
 
